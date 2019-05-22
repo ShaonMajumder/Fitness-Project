52f09cf2e493e68a514689966a652da4eef5bb98
Shaon Majumder <shaon.fdr@robist.com>
2019-05-23 00:02:20 +0600

    user_profile_form modularize

--- python-gui-login.py
+++ python-gui-login.py
@@ -16,9 +16,6 @@ global session_data
 session_data = {}
 
 
-def set_session(profile_id):
-    login_state = True
-    session_data['profile_id'] = profile_id
 
 def destroy_session():
     login_state = False
@@ -63,68 +60,9 @@ def select_file():
 
 # Designing window for registration
  
-def register():
-    global register_screen
-    register_screen = Toplevel(main_screen)
-    register_screen.title("Register")
-    register_screen.geometry("300x250")
- 
-    global username
-    global password
-    global email
-    global username_entry
-    global password_entry
-    global email_entry
-    username = StringVar()
-    password = StringVar()
-    email = StringVar()
- 
-    Label(register_screen, text="Please enter details below", bg="blue").pack()
-    Label(register_screen, text="").pack()
-    username_lable = Label(register_screen, text="Username * ")
-    username_lable.pack()
-    username_entry = Entry(register_screen, textvariable=username)
-    username_entry.pack()
-    email_lable = Label(register_screen, text="Email * ")
-    email_lable.pack()
-    email_entry = Entry(register_screen, textvariable=email)
-    email_entry.pack()
-    password_lable = Label(register_screen, text="Password * ")
-    password_lable.pack()
-    password_entry = Entry(register_screen, textvariable=password, show='*')
-    password_entry.pack()
-    Label(register_screen, text="").pack()
-    Button(register_screen, text="Register", width=10, height=1, bg="blue", command = register_user).pack()
- 
  
 # Designing window for login 
  
-def login():
-    global login_screen
-    login_screen = Toplevel(main_screen)
-    login_screen.title("Login")
-    login_screen.geometry("300x250")
-    Label(login_screen, text="Please enter details below to login").pack()
-    Label(login_screen, text="").pack()
- 
-    global email_verify
-    global password_verify
- 
-    email_verify = StringVar()
-    password_verify = StringVar()
- 
-    global email_login_entry
-    global password_login_entry
- 
-    Label(login_screen, text="Email * ").pack()
-    email_login_entry = Entry(login_screen, textvariable=email_verify)
-    email_login_entry.pack()
-    Label(login_screen, text="").pack()
-    Label(login_screen, text="Password * ").pack()
-    password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
-    password_login_entry.pack()
-    Label(login_screen, text="").pack()
-    Button(login_screen, text="Login", width=10, height=1, command = login_verify).pack()
  
 def save_profile_data():
     profile_id = session_data['profile_id']
@@ -226,76 +164,16 @@ def add_profile_details():
     activity_level_entry.pack()
     Button(add_profile_details_screen, text="Submit", command=save_profile_data).pack()
 
-# Implementing event on register button
  
-def register_user(): 
-    username_info = username.get()
-    email_info = email.get()
-    password_info = password.get()
-    password_result = hashlib.md5(password_info.encode())
-    password_info = password_result.hexdigest()
-    
-    #validation required
-    result_email = mydb.select('*',"`email` = '"+email_info+"'",'profiles')
-    result_username = mydb.select('*',"`username` = '"+username_info+"'",'profiles')
-    if(result_email == () and result_username == ()):
-        while(True):
-            random_key = randomString(stringLength=8)
-            result = mydb.select("*",f"""`profile_id` = '{random_key}'""","profiles")
-            if result != ():
-                pass
-            else:
-                break
 
-        mydb.insert(['profile_id','username','email','password'],[random_key,username_info,email_info,password_info],'profiles')
-        sent_from = gmail_user  
-        to = [email_info]
-        subject = 'Activate Account - ROBIST'
-        body = f"""Hey {username_info}, thanks for register with Robist account. To activate your account, click here or visit this link - .\n\n- Robist Automated Reply"""
-        CC = ''
-        send_email(subject, body, CC, to, sent_from, gmail_user, gmail_password)
-
-    elif(result_email != ()):
-        print("This email is already registered.")
-    elif(result_username != ()):
-        print("Username already taken.")
+def user_profile_form(session_data):
+    global user_profile_screen
+    user_profile_screen = Tk() #Toplevel(object_)
+    user_profile_screen.title("User Details")
+    user_profile_screen.geometry("350x250")
+    user_profile_screen.columnconfigure(0, weight=350)
     
 
-    username_entry.delete(0, END)
-    email_entry.delete(0, END)
-    password_entry.delete(0, END)
- 
-    Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
- 
-# Implementing event on login button 
- 
-def login_verify():
-    email1 = email_verify.get()
-    password1 = password_verify.get()
-    password_result = hashlib.md5(password1.encode())
-    password1 = password_result.hexdigest()
-
-    email_login_entry.delete(0, END)
-    password_login_entry.delete(0, END)
-
-    username_result = mydb.select('*',f"""`email`='{email1}'""","profiles")
-    if username_result != ():
-        password_result = mydb.select('*',f"""`password`='{password1}'""","profiles")
-        if password_result != ():
-            profile_id = password_result[0]['profile_id']
-            set_session(profile_id)
-            #main_screen.withdraw()
-            #main_screen.deiconify()
-            messagebox.showinfo("Fitness-Project", "You are successfully logged in.")
-            login_screen.destroy()
-            user_profile_form()
-        else:
-            messagebox.showinfo("Fitness-Project", "Password does not match.")
-    else:
-        messagebox.showinfo("Fitness-Project", "This email is not registered with us.\nEnter your correct email.")
- 
-
-def user_profile_form():
     profile_id = session_data['profile_id']
     results = mydb.execute(f"""SELECT * FROM biodata INNER JOIN profiles ON biodata.profile_id = profiles.profile_id WHERE profiles.`profile_id` = '{profile_id}'""")
     row = results[0]
@@ -316,18 +194,14 @@ Body Weight: {bodyweight}
 Meal Number: {meal_number}
 Activity Level: {activity_level}"""
 
-    global user_profile_screen
-    user_profile_screen = Toplevel(main_screen)
-    user_profile_screen.title("User Details")
-    user_profile_screen.geometry("350x250")
-    user_profile_screen.columnconfigure(0, weight=350)
+    
 
     profile_picture_folder="imgs/"
     if profile_picture == "":
         img_file = "profile_avatar.png"
     else:
         img_file = profile_picture
-
+    
     original = PIL.Image.open(profile_picture_folder + img_file)
     size = (90, 90)
     resized = original.resize(size,PIL.Image.ANTIALIAS)
@@ -372,23 +246,23 @@ Activity Level: {activity_level}"""
     Label(pro_pic_frame,textvariable=meal_number, width =5, bg='#c1b3aa').grid(row=5,column=2,sticky="w")
     Label(pro_pic_frame,textvariable=activity_level, width =5, bg='#c1b3aa').grid(row=6,column=2,sticky="w")
 
-    
-
 
     #panel2 = Label(pro_pic_frame,text=user_details, bg='red')
     #panel2.grid(row=1,column=1,sticky="nesw")
     Button(user_profile_screen,text="Add/Edit details", height="2", width="30", command = add_profile_details).grid(row=2,sticky="nesw")
     Button(pro_pic_frame,text="Change", command = select_file).grid(row=6,column=0,sticky="w")
     #Button(pro_pic_frame,text="Change", command = lambda: select_file(new_propic_filename)).grid(row=6,column=0,sticky="w")
-
- 
-
+    
+    return user_profile_screen
 
 class Login_Register(object):
     """docstring for ClassName"""
     def __init__(self,*args, **kwargs):
         ## object,iconfile
         iconfile = kwargs['icon']
+        self.session_data = session_data
+        self.success_function = kwargs['success_function']
+        self.login_state = False
 
         if 'object' in kwargs:
             #   type(Toplevel(Tk())).__name__ == 'Toplevel'
@@ -403,17 +277,174 @@ class Login_Register(object):
         main_screen.configure(background='#84A45A')
 
         Label(text="Fitness-Project", bg="#715E4E", fg='white', width="300", height="2", font=("Calibri", 13)).pack()
-        Button(text="Login", bg='#818A6F', fg='white', height="2", width="30", command = login).pack(pady=20)
-        Button(text="Register", bg='#52733B', fg='white', height="2", width="30", command = register).pack()
-        self.frame = main_screen    
+        Button(text="Login", bg='#818A6F', fg='white', height="2", width="30", command = lambda: self.login()).pack(pady=20)
+        Button(text="Register", bg='#52733B', fg='white', height="2", width="30", command = lambda: self.register()).pack()
+        self.frame = main_screen
+
+    def register_user(self):
+        username_info = username.get()
+        email_info = email.get()
+        password_info = password.get()
+        password_result = hashlib.md5(password_info.encode())
+        password_info = password_result.hexdigest()
+        
+        #validation required
+        result_email = mydb.select('*',"`email` = '"+email_info+"'",'profiles')
+        result_username = mydb.select('*',"`username` = '"+username_info+"'",'profiles')
+        if(result_email == () and result_username == ()):
+            while(True):
+                random_key = randomString(stringLength=8)
+                result = mydb.select("*",f"""`profile_id` = '{random_key}'""","profiles")
+                if result != ():
+                    pass
+                else:
+                    break
+
+            mydb.insert(['profile_id','username','email','password'],[random_key,username_info,email_info,password_info],'profiles')
+            sent_from = gmail_user  
+            to = [email_info]
+            subject = 'Activate Account - ROBIST'
+            body = f"""Hey {username_info}, thanks for register with Robist account. To activate your account, click here or visit this link - .\n\n- Robist Automated Reply"""
+            CC = ''
+            send_email(subject, body, CC, to, sent_from, gmail_user, gmail_password)
+
+        elif(result_email != ()):
+            print("This email is already registered.")
+        elif(result_username != ()):
+            print("Username already taken.")
+        
+
+        username_entry.delete(0, END)
+        email_entry.delete(0, END)
+        password_entry.delete(0, END)
+     
+        Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+ 
+    def login_verify(self):
+        email1 = self.email_verify.get()
+        password1 = self.password_verify.get()
+        password_result = hashlib.md5(password1.encode())
+        password1 = password_result.hexdigest()
+        email_login_entry = self.email_login_entry
+        password_login_entry = self.password_login_entry
+        email_login_entry.delete(0, END)
+        password_login_entry.delete(0, END)
+
+        username_result = mydb.select('*',f"""`email`='{email1}'""","profiles")
+        if username_result != ():
+            password_result = mydb.select('*',f"""`password`='{password1}'""","profiles")
+            if password_result != ():
+                profile_id = password_result[0]['profile_id']
+                self.set_session(profile_id)
+                self.login_screen.destroy()
+                messagebox.showinfo("Fitness-Project", "You are successfully logged in.")
+                
+                #main_screen.withdraw()
+                #main_screen.deiconify()
+
+                #self.success_function()
+                #self.frame.destroy()
+                
+                
+            else:
+                messagebox.showinfo("Fitness-Project", "Password does not match.")
+        else:
+            messagebox.showinfo("Fitness-Project", "This email is not registered with us.\nEnter your correct email.")
+
+    def register(self):
+        #global register_screen
+        register_screen = Toplevel(main_screen)
+        register_screen.title("Register")
+        register_screen.geometry("300x250")
+
+        #global username
+        #global password
+        #global email
+        #global username_entry
+        #global password_entry
+        #global email_entry
+        
+        username = StringVar()
+        password = StringVar()
+        email = StringVar()
+     
+        Label(register_screen, text="Please enter details below", bg="blue").pack()
+        Label(register_screen, text="").pack()
+        username_lable = Label(register_screen, text="Username * ")
+        username_lable.pack()
+        username_entry = Entry(register_screen, textvariable=username)
+        username_entry.pack()
+        email_lable = Label(register_screen, text="Email * ")
+        email_lable.pack()
+        email_entry = Entry(register_screen, textvariable=email)
+        email_entry.pack()
+        password_lable = Label(register_screen, text="Password * ")
+        password_lable.pack()
+        password_entry = Entry(register_screen, textvariable=password, show='*')
+        password_entry.pack()
+        Label(register_screen, text="").pack()
+        Button(register_screen, text="Register", width=10, height=1, bg="blue", command = lambda: self.register_user()).pack()
+        self.register_screen = register_screen
+        self.username = username
+        self.password = password
+        self.email = email
+        self.username_entry = username_entry
+        self.password_entry = password_entry
+        self.email_entry = email_entry
+
+    def login(self):
+        #global login_screen
+        login_screen = Toplevel(self.frame)
+        login_screen.title("Login")
+        login_screen.geometry("300x250")
+        Label(login_screen, text="Please enter details below to login").pack()
+        Label(login_screen, text="").pack()
+     
+        #global email_verify
+        #global password_verify
+     
+        email_verify = StringVar()
+        password_verify = StringVar()
+     
+        #global email_login_entry
+        #global password_login_entry
+     
+        Label(login_screen, text="Email * ").pack()
+        email_login_entry = Entry(login_screen, textvariable=email_verify)
+        email_login_entry.pack()
+        Label(login_screen, text="").pack()
+        Label(login_screen, text="Password * ").pack()
+        password_login_entry = Entry(login_screen, textvariable=password_verify, show= '*')
+        password_login_entry.pack()
+        Label(login_screen, text="").pack()
+        Button(login_screen, text="Login", width=10, height=1, command = lambda: self.login_verify()).pack()
+        self.login_screen = login_screen
+        self.email_verify = email_verify
+        self.password_verify = password_verify
+        self.email_login_entry = email_login_entry
+        self.password_login_entry = password_login_entry
+    def is_login(self):
+        return self.login_state
+    def set_session(self,profile_id):
+        self.login_state = True
+        self.session_data = {'profile_id': profile_id}
+        print(self.session_data)
+
 
 # Designing Main(first) window
 def main_account_screen():
+    """
     global main_screen
-    object_ = Login_Register(icon="imgs/robist_apps.ico")
+    object_ = Login_Register(icon="imgs/robist_apps.ico",success_function=lambda: user_profile_form())
     main_screen = object_.frame
     main_screen.mainloop()
- 
+    while object_.is_login() == False:
+        pass
+    main_screen.destroy()
+    """
+    user_profile_for = user_profile_form( {'profile_id': "vwjjn6ip"})
+    user_profile_for.mainloop()
+    
 main_account_screen()
 
 """
